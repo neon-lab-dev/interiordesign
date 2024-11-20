2c5505d5726262ead878668624678206e4bae420
adhikareeprayush <adhikareeprayush@gmail.com>
2024-11-20 22:19:09 +0545

    some fixes

--- src/Components/DashboardPages/AccountDetailsPage.jsx
+++ src/Components/DashboardPages/AccountDetailsPage.jsx
@@ -88,82 +88,87 @@ const AccountDetailsPage = () => {
         <div className="content-sections">
           <Sidebar />
           <div className="right-section">
-          <div className="account-details-card">
-            <h3 className="account-details-card-title">Account Details</h3>
-            <span className="edit-icon" onClick={toggleEditMode}>
-              <FaRegEdit />
-            </span>
-            <div className="img-Name">
-              <img
-                src={userDetails.profilePic}
-                alt="Profile"
-                className="profile-pic"
-              />
-              <span className="c">{userDetails.name}</span>
+            <div className="account-details-card">
+              <h3 className="account-details-card-title">Account Details</h3>
+              <span className="edit-icon" onClick={toggleEditMode}>
+                <FaRegEdit />
+              </span>
+              <div className="img-Name">
+                <img
+                  src={userDetails.profilePic}
+                  alt="Profile"
+                  className="profile-pic"
+                />
+                <span className="c">{userDetails.name}</span>
+              </div>
+              <div className="d-flex flex-column profileContacts gap-1">
+                <p>Email: {userDetails.email}</p>
+                <p>Phone: {userDetails.phone}</p>
+              </div>
             </div>
-            <div className="d-flex flex-column profileContacts gap-1">
-              <p>Email: {userDetails.email}</p>
-              <p>Phone: {userDetails.phone}</p>
-            </div>
+
+            {isEditing && (
+              <div className="edit-form">
+                <form>
+                  <div className="name-phone">
+                    <div className="form-group">
+                      <label>Full Name*</label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={userDetails.name}
+                        onChange={handleChange}
+                        className="form-control"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Phone*</label>
+                      <input
+                        type="tel"
+                        name="phone"
+                        value={userDetails.phone}
+                        onChange={handleChange}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="name-phone">
+                    <div className="form-group">
+                      <label>Email*</label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={userDetails.email}
+                        onChange={handleChange}
+                        className="form-control"
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label>Upload pic</label>
+                      <div className="input-field-container d-flex gap-1 align-items-center">
+                        <label htmlFor="profilePic">Choose File</label>
+                        <span>No file chosen</span>
+                      </div>
+                      <input
+                        type="file"
+                        name="profilePic"
+                        id="profilePic"
+                        onChange={handleFileChange}
+                        className="form-control upload-input d-none"
+                      />
+                    </div>
+                  </div>
+
+                  <button type="button" onClick={handleSave} className="save-btn">
+                    Update
+                  </button>
+                </form>
+              </div>
+            )}
           </div>
 
-          {isEditing && (
-            <div className="edit-form">
-              <form>
-                <div className="name-phone">
-                  <div className="form-group">
-                    <label>Full Name*</label>
-                    <input
-                      type="text"
-                      name="name"
-                      value={userDetails.name}
-                      onChange={handleChange}
-                      className="form-control"
-                    />
-                  </div>
-                  <div className="form-group">
-                  <label>Phone*</label>
-                  <input
-                    type="tel"
-                    name="phone"
-                    value={userDetails.phone}
-                    onChange={handleChange}
-                    className="form-control"
-                  />
-                </div>
-                </div>
-
-                <div className="name-phone">
-                <div className="form-group">
-                    <label>Email*</label>
-                    <input
-                      type="email"
-                      name="email"
-                      value={userDetails.email}
-                      onChange={handleChange}
-                      className="form-control"
-                    />
-                  </div>
-                
-                <div className="form-group">
-                  <label>Upload pic</label>
-                  <input
-                    type="file"
-                    name="profilePic"
-                    onChange={handleFileChange}
-                    className="form-control"
-                  />
-                </div>
-                </div>
-                
-                <button type="button" onClick={handleSave} className="save-btn">
-                  Update
-                </button>
-              </form>
-            </div>
-          )}
-          </div>
-          
         </div>
       </div>
     </section>
