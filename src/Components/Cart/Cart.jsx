9df2798a1efe97e0df721526071667990dcbce58
adhikareeprayush <adhikareeprayush@gmail.com>
2024-12-17 11:37:29 +0545

    cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,30 +1,5 @@
-// import React from 'react'
-// import './Cart.css'
-// import Card from './Card'
-// import BestSellers from '../BestSellers/BestSellers'
-// import ProductLike from '../ProductLike/ProductLike'
-
-//  const Cart = () => {
-//   return (
-//     <div>
-//       <div className='cart-container'>
-//       <p className='items'>Your Cart :- 2 items</p>
-
-      
-//       <Card />
-      
-//     </div>
-//     <ProductLike />
-//     </div>
-    
-//   )
-// }
-// export default Cart;
-
-
-import React, { useEffect, useState } from "react";
-import "./Cart.css";
-import CardCart from "./CardCart";
+import { useEffect, useState } from "react";
+import bed from "../../assets/Images/bedsheet.png"
 
 const Cart = () => {
   const [cartProducts, setCartProducts] = useState([]);
@@ -37,17 +12,65 @@ const Cart = () => {
 
   return (
     <div>
-      <div className="cart-container">
-        <p className="items">Your Cart: {cartProducts.length} items</p>
-        {/* Render a Card for each product */}
-        {cartProducts.length > 0 ? (
-          cartProducts.map((product, index) => (
-            <CardCart key={index} product={product} setCartProducts={setCartProducts} />
-          ))
-        ) : (
-          <p className="empty-cart">Your cart is empty.</p>
-        )}
-      </div>
+      <section className="pt-4">
+        <h4 className="items fw-medium">Your Cart: {cartProducts.length} items</h4>
+        <div className="w-100 d-flex justify-content-between gap-5 flex-lg-row flex-column">
+          <div className="d-flex flex-md-row flex-column align-items-center gap-3 border border-secondary rounded-2 shadow shadow-md overflow-hidden p-2 flex-grow-1" style={{ height: "280px" }}>
+            <div className="d-flex items-center justify-center bg-image p-1 rounded-2 ">
+              <img src={bed} alt="" className="rounded-1" style={{ height: '250px' }} />
+            </div>
+            <div className="d-flex flex-column justify-content-between h-100 items-start">
+              <div className="d-flex flex-column">
+                <p className="text-lg fw-normal"><span className="fw-medium">Product :</span> English Willow Seasoned Bat</p>
+                <span>- Ventue Sky Blue</span>
+              </div>
+              <div className="d-flex items-start flex-column gap-1">
+                <span><b>Quantity: 1</b></span>
+                <div className="d-flex items-center quantity-btns">
+                  <button className="btn btn-transparent">-</button>
+                  <button className="btn btn-transparent">1</button>
+                  <button className="btn btn-transparent">+</button>
+                </div>
+              </div>
+              <div className="d-flex items-center gap-2">
+                <span className="text-muted">₹5000.00</span>
+                <span className="price">₹3750.00</span>
+                <span className="discount">25% off</span>
+              </div>
+              <button className="btn btn-transparent w-100">Remove</button>
+            </div>
+          </div>
+          <div className="d-flex flex-column items-start border border-secondary rounded-2 shadow shadow-md overflow-hidden p-2" style={{ minWidth: '340px' }}>
+            <h5 className="fw-light text-uppercase border-bottom pb-1">Price details</h5>
+            <div className="d-flex flex-column items-start gap-2 border-bottom py-2">
+              <div className="d-flex justify-content-between w-100">
+                <span>Price (1 item)</span>
+                <span className="price">₹3750.00</span>
+              </div>
+
+              <div className="d-flex justify-content-between w-100">
+                <span>Discount</span>
+                <span className="discount">- ₹1250.00</span>
+              </div>
+              <div className="d-flex justify-content-between w-100">
+                <span>Delivery Charges</span>
+                <span>Free</span>
+              </div>
+            </div>
+            <div className="d-flex justify-content-between w-100 py-2 border-bottom">
+              <span>Total Amount</span>
+              <span className="price">₹3750.00</span>
+            </div>
+            <div className="d-flex flex-column align-items-start gap-2 pt-2">
+              <div className="d-flex">
+                <p>You will save <span className="discount d-inline">₹1250.00</span> on this order</p>
+              </div>
+              <button className="btn btn-lg-colored w-100 py-2">Place Order</button>
+            </div>
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 };
