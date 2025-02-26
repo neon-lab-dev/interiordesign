5ad35fbe7fb761431efd3ef913426edc82c0305d
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-26 13:45:04 +0600

    Removed whitespaces

--- src/Components/Nav/NavHome.jsx
+++ src/Components/Nav/NavHome.jsx
@@ -21,37 +21,37 @@ const Nav = () => {
           <Link to="/" className="logo">
             <img src={IMAGES.footerLogo} alt="logo" />
           </Link>
-         
+
         </div>
         <div className="d-flex align-items-center gap-4 left-container">
-        <div className="nav-links d-flex align-items-center gap-3 horizontal-nav-links">
-        <NavLink 
-        to="/" 
-        className={({ isActive }) => 
-          isActive ? "active-link border-bottom border-2 " : "inactive-link"
-        }
-      >
-        Home
-      </NavLink>
-      <NavLink   
-        to="/about-us" 
-        className={({ isActive }) => 
-          isActive ? "active-link border-bottom border-2 text-nowrap " : "inactive-link text-nowrap"
-        }
-      >
-        About Us
-      </NavLink>
-      <NavLink   
-        to="/bedsheets" 
-        className={({ isActive }) => 
-          isActive ? "active-link border-bottom border-2" : "inactive-link"
-        }
-      >
-        Products
-      </NavLink>
+          <div className="nav-links d-flex align-items-center gap-3 horizontal-nav-links">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "active-link border-bottom border-2 " : "inactive-link"
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about-us"
+              className={({ isActive }) =>
+                isActive ? "active-link border-bottom border-2 text-nowrap " : "inactive-link text-nowrap"
+              }
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/bedsheets"
+              className={({ isActive }) =>
+                isActive ? "active-link border-bottom border-2" : "inactive-link"
+              }
+            >
+              Products
+            </NavLink>
             <button type="submit" className="w-100 custom-text-2  btnNav">
-                    Get Free Quote
-                  </button>
+              Get Free Quote
+            </button>
           </div>
           <div className="d-flex align-items-center gap-4 left-menus">
             <div
@@ -65,14 +65,14 @@ const Nav = () => {
       </div>
       {/* Sidebar */}
       <div className={`sidebar-ham ${isSidebarOpen ? "open" : ""}`}>
-        
+
         <div className="sidebar-links">
           <Link to="/">Home</Link>
           <Link to="/about-us">About Us</Link>
           <Link to="/bedsheets">Products</Link>
         </div>
-        
-      
+
+
       </div>
       <div
         className={`backdrop ${isSidebarOpen ? "active" : ""}`}
